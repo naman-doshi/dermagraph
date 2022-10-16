98143951ef07f708acc091d695769d7ded1a1e3a
aerobull42 <doshinaman2006@gmail.com>
2022-10-16 21:20:42 +1100

    minor updates

---
+++ src/train.py
@@ -0,0 +1,8 @@
+import pandas as pd, numpy as np
+from kaggle_datasets import KaggleDatasets
+import tensorflow as tf, re, math
+import tensorflow.keras.backend as K
+import efficientnet.tfkeras as efn
+from sklearn.model_selection import KFold
+from sklearn.metrics import roc_auc_score
+import matplotlib.pyplot as plt
